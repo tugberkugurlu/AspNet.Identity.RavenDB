c323707e05553d7d3ef5f4682921d4e948e0cbbd
tugberkugurlu <tugberkugurlu@gmail.com>
2013-07-28 13:44:56 +0300

    added common assembly info

---
+++ src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Tugberk Ugurlu")]
+[assembly: AssemblyCopyright("Copyright © Tugberk Ugurlu 2013")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-pre01")]
