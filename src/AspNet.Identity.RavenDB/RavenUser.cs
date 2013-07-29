6ea80414505eb6781b91ed7890dc3ee071727ac5
tugberkugurlu <tugberkugurlu@gmail.com>
2013-07-29 16:47:07 +0300

    started adding RavenUserLoginStoreFacts

--- src/AspNet.Identity.RavenDB/RavenUser.cs
+++ src/AspNet.Identity.RavenDB/RavenUser.cs
@@ -1,10 +1,18 @@
 ﻿using AspNet.Identity.RavenDB.Entities;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace AspNet.Identity.RavenDB
 {
     public class RavenUser : User
     {
+        public RavenUser()
+        {
+            Roles = new Collection<Role>();
+            UserClaims = new Collection<UserClaim>();
+            UserLogins = new Collection<UserLogin>();
+        }
+
         public UserSecret Secret { get; set; }
         public ICollection<Role> Roles { get; set; }
         public ICollection<UserClaim> UserClaims { get; set; }
--- tests/AspNet.Identity.RavenDB.Tests/Stores/RavenUserLoginStoreFacts.cs
+++ tests/AspNet.Identity.RavenDB.Tests/Stores/RavenUserLoginStoreFacts.cs
@@ -11,26 +11,27 @@ using Xunit;
 
 namespace AspNet.Identity.RavenDB.Tests.Stores
 {
-    public class RavenUserSecretStoreFacts : TestBase
+    public class RavenUserLoginStoreFacts : TestBase
     {
-        [Fact(Skip = "Issue #1 blocks this test")]
-        public async Task Create_Should_Create_Secret_If_User_Exists_And_Secret_Does_Not()
+        [Fact]
+        public async Task Add_Should_Add_New_Login_If_User_Exists()
         {
             string userName = "Tugberk";
-            string userSecret = "1234567890qwertyuiop";
 
             using (IDocumentStore store = CreateEmbeddableStore())
             using (IAsyncDocumentSession ses = store.OpenAsyncSession())
             {
-                IUserSecretStore userSecretStore = new RavenUserSecretStore<RavenUser, UserSecret>(ses);
+                IUserLoginStore userLoginStore = new RavenUserLoginStore<RavenUser, UserLogin>(ses);
                 await ses.StoreAsync(new RavenUser { Id = "RavenUsers/1", UserName = userName });
-                bool result = await userSecretStore.Create(new UserSecret { UserName = userName, Secret = userSecret });
+                await ses.SaveChangesAsync();
+
+                bool result = await userLoginStore.Add(new UserLogin { UserId = "RavenUsers/1", LoginProvider = "Local", ProviderKey = userName });
                 await ses.SaveChangesAsync();
 
                 RavenUser user = await ses.LoadAsync<RavenUser>("RavenUsers/1");
 
                 Assert.True(result);
-                Assert.NotNull(user.Secret);
+                Assert.Equal(1, user.UserLogins.Count);
             }
         }
     }
