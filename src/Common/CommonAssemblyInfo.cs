9d8c05c37fc70c004e23aa23859c8cf1b0157714
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-10-22 20:01:12 +0200

    incremented version

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-pre03")]
+[assembly: AssemblyInformationalVersion("1.0.0-pre04")]
