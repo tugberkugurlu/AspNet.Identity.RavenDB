377bae05098f7fb9b7dfffabf1caa395ad85c7fd
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-13 18:01:43 +0200

    added RavenUserEmailConfirmation

---
+++ src/AspNet.Identity.RavenDB/Entities/RavenUserEmailConfirmation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AspNet.Identity.RavenDB.Entities.Entities
+{
+    /// <summary>
+    /// As we need to make the e-mail confirmation happen only once, this should be 
+    /// in a seperate document with a distinct identifier which is RavenUsers/{userName}/{email}
+    /// </summary>
+    public class RavenUserEmailConfirmation
+    {
+        public RavenUserEmailConfirmation()
+        {
+        }
+
+        public RavenUserEmailConfirmation(string userName, string email)
+        {
+            Id = string.Concat("RavenUsers/", userName, "/", email);
+        }
+
+        public string Id { get; set; }
+        public DateTimeOffset ConfirmedOn { get; set; }
+    }
+}
