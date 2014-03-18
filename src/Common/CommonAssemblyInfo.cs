42f827932482f496823e4f0ae52a556ecca54af8
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-18 17:40:01 +0200

    bumped the version to 2.0.0-beta1-01

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -1,8 +1,5 @@
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyCompany("Tugberk Ugurlu")]
 [assembly: AssemblyCopyright("Copyright © Tugberk Ugurlu 2013")]
 
@@ -10,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta1-01")]
